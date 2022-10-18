ae5349466511ef0ddc61f9b4d47cb103fcc7c687
Gustavo Sobral <gustavosoaressobral@gmail.com>
2022-10-18 20:42:30 +0100

    Create an event that paints the grid where the mouse passes over

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-//I want to crate a grid of 16x16px using javascript
-//when the mouse hover's over the divs, they change color to black
+//I want to create a grid of 16x16px using javascript
+//when the mouse hovers over the divs, they change color to black
 
 createGrid();
+const container = document.querySelector(".container");
 
 function createGrid(){
     const container = document.querySelector(".container");
-    const gridWidth = container.offsetWidth;
+    const gridWidth = container.clientWidth;
     let newDiv;
-    
+
     for(let i = 0; i < gridWidth; i++){
         newDiv = document.createElement("div");
         container.appendChild(newDiv);
     }
 }
+
+//when the mouse hovers over a div, it changes color to black
+function paintDiv(){
+    
+}
+
+addEventListener("mouseover", (e) => {
+    if(e.target.parentElement === container)
+        e.target.classList.add("painted-div");
+});
