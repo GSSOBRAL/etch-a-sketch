2e6190c055938c7e9b1c657ff7b31e8cfc827392
Gustavo Sobral <gustavosoaressobral@gmail.com>
2022-10-24 19:19:41 +0100

    Create a button for the user to choose a new size for the grid

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 //I want to create a grid of 16x16px using javascript
 //when the mouse hovers over the divs, they change color to black
 
-createGrid();
 const container = document.querySelector(".container");
+const gridSizeButton = document.querySelector(".grid-size-btn");
+
+createGrid();
+
+container.addEventListener("mouseover", paintDiv);
+gridSizeButton.addEventListener("click", chooseGridSize);
 
 function createGrid(){
-    const container = document.querySelector(".container");
     const gridWidth = container.clientWidth;
     let newDiv;
 
-    for(let i = 0; i < gridWidth; i++){
+    for(let i = 0; i < gridWidth * 2; i++){
         newDiv = document.createElement("div");
         container.appendChild(newDiv);
     }
 }
 
-//when the mouse hovers over a div, it changes color to black
-function paintDiv(){
-    
+function paintDiv(event){
+    if(event.target.parentElement === container)
+        event.target.classList.add("painted-div");
 }
 
-addEventListener("mouseover", (e) => {
-    if(e.target.parentElement === container)
-        e.target.classList.add("painted-div");
-});
+function chooseGridSize(){
+    let newGridSize = +prompt("What size in pixels do you want your grid?");
+}
